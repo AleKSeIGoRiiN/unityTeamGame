033a5c30a978fc29dbdb5224abc75582159b99f8
SyrykhSergey <syrykh.ser@gmail.com>
2021-06-20 01:30:16 +0700

    анимации атаки и атака

---
+++ Brave Dive/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+
+    public Animator animator;
+    public Transform attackPoint;
+    public float attackRange = 2.0f;
+    public LayerMask enemyLayers;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Attack();
+        }
+        
+    }
+    void Attack()
+    {
+        animator.SetTrigger("Attack");
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("we hit" + enemy.name);
+        }
+    }
+}
