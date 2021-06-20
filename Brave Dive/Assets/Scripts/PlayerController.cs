f6ecaaa2a23398c5ceaaf8369fd27cd7498375c9
SyrykhSergey <syrykh.ser@gmail.com>
2021-06-20 23:33:03 +0700

    вернул старую систему управления

--- Brave Dive/Assets/Scripts/PlayerController.cs
+++ Brave Dive/Assets/Scripts/PlayerController.cs
@@ -3,8 +3,9 @@ using System;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 400.0f;
+    public float speed = 15.0f;
     public Camera cam; // переменка для камеры
+    
     private Rigidbody2D _body; // создаём под тело
 
     Vector2 movement = Vector2.zero;// вектор движений, содержит дельты х & y
@@ -18,87 +19,20 @@ public class PlayerController : MonoBehaviour
     // 
     void Update()
     {
+        speed = 15.0f;
         float time;
 
 
         _body = GetComponent<Rigidbody2D>();// Делаем тельце твердым
         movement.x = Input.GetAxis("Horizontal");
         movement.y = Input.GetAxis("Vertical");//Принимает нажатия (стрелки и wasd)
+        if (Math.Abs(movement.y) == Math.Abs(movement.x)) speed -= 3;
 
 
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);// хаваем - камера.Перевести в координаты (позиция мышки)
         lookDir = mousePos - _body.position;
 
-        movementVectors = lookDir;
-        movementVectors = movementVectors.normalized;
-
-
-
-
-        if (movement.y < 0)
-        {
-            movementVectors.x = -movementVectors.x;
-            movementVectors.y = -movementVectors.y;
-            if (movement.x < 0)
-            {
-                time = movementVectors.x;
-                movementVectors.x = movementVectors.x + movementVectors.y;
-                movementVectors.y = movementVectors.y - time;
-                movementVectors = movementVectors.normalized;
-            }
-
-            else if (movement.x > 0)
-            {
-                time = movementVectors.x;
-                movementVectors.x = movementVectors.x - movementVectors.y;
-                movementVectors.y += time;
-                movementVectors = movementVectors.normalized;
-
-            }
-
-        }
-
-        else if (movement.y > 0)
-        {
-            if (movement.x > 0)
-            {
-                time = movementVectors.x;
-                movementVectors.x = movementVectors.x + movementVectors.y;
-                movementVectors.y = movementVectors.y - time;
-                movementVectors = movementVectors.normalized;
-            }
-
-            else if (movement.x < 0)
-            {
-                time = movementVectors.x;
-                movementVectors.x = movementVectors.x - movementVectors.y;
-                movementVectors.y += time;
-                movementVectors = movementVectors.normalized;
-
-            }
-
-        }
-
-        else if (movement.x < 0)
-        {
-            time = movementVectors.x;
-            movementVectors.x = -movementVectors.y;
-            movementVectors.y = time;
-
-        }
-        else if (movement.x > 0)
-        {
-
-            time = movementVectors.x;
-            movementVectors.x = movementVectors.y;
-            movementVectors.y = -time;
-
-        }
-        else if (movement.x == 0 && movement.y == 0)
-        {
-            movementVectors.x = 0;
-            movementVectors.y = 0;
-        }
+        
 
         //#######################################################################################################################
 
@@ -132,14 +66,10 @@ public class PlayerController : MonoBehaviour
 
 
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg + 90f;   //А здесь чисто чуток матеши p.s. этот кусок не двигать!
-        _body.rotation = angle + 5f;
+        _body.rotation = angle -10f;
 
 
-
-
-
-
-        _body.MovePosition(_body.position + movementVectors * speed * Time.fixedDeltaTime);//Само сдвижение (тайм фиксед, чтобы было независимо от кол-во фпс)
+        _body.MovePosition(_body.position + movement * speed * Time.fixedDeltaTime);//Само сдвижение (тайм фиксед, чтобы было независимо от кол-во фпс)
 
 
 
