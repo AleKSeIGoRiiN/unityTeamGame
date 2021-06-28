617461fb461c6584fcb7d7416c6b7fe62c29d555
Stepan2002sas <63295392+Stepan2002sas@users.noreply.github.com>
2021-06-28 16:16:55 +0700

    сделал сцену смерти пофиксил меню и еще что-то

---
+++ Brave Dive/Assets/AllMenu/AboutGameMenu.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AboutGameMenu : MonoBehaviour
+{
+    public void Share()
+    {
+        Application.OpenURL("https://www.instagram.com/_porrnoo_gey_/?hl=ru");
+    }
+}
